13fea837d8c555dd364ea11c946d011f25ef79df
MrHaCkEr <MrHaCkEr@users.noreply.github.com>
2018-02-11 22:42:14 +0100

    Update index.js

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,7 +5,7 @@ import styles from './index.module.css';
 const AppLayout = ({ children }) => (
   <div className={styles.container}>
     <h1>
-      <Link to="/">Welcome to my blog :)</Link>
+      <Link to="/">Welcome to my blog</Link>
     </h1>
     {children()}
   </div>
